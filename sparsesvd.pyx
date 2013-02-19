5568d5a30e113dba5acc8b857a32ed165ca9a5f9
Alejandro Pulver <alepulver@gmail.com>
2013-02-19 20:35:05 -0300

    - Clean up Cython code

--- sparsesvd.pyx
+++ sparsesvd.pyx
@@ -1,9 +1,9 @@
 # cython: infer_types=True
+
 from scipy.sparse import issparse, isspmatrix_csc
 cimport numpy as np
 import numpy as np
 from libc.stdlib cimport free
-from cython cimport view
 
 def sparsesvd(matrix, k):
     if isspmatrix_csc(matrix):
@@ -19,37 +19,43 @@ def sparsesvd(matrix, k):
     mat.rows = matrix.shape[0]
     mat.cols = matrix.shape[1]
     mat.vals = matrix.nnz
+
+    """
+    # old style
     cdef np.ndarray[long, mode = 'c'] indptr = np.ascontiguousarray(matrix.indptr, dtype=np.long)
     cdef np.ndarray[long, mode = 'c'] indices = np.ascontiguousarray(matrix.indices, dtype=np.long)
     cdef np.ndarray[double, mode = 'c'] data = np.ascontiguousarray(matrix.data, dtype=np.double)
-    #cdef double [:] data = np.ascontiguousarray(matrix.data, dtype=np.double)
-    # TODO: type corversion may be needed?
-    #cdef double [:] data = matrix
     mat.pointr = <long *>indptr.data
     mat.rowind = <long *>indices.data
     mat.value = <double *>data.data
-    #mat.value = &data[0]
+    """
+
+    cdef long [:] indptr = np.ascontiguousarray(matrix.indptr, dtype=np.long).data
+    cdef long [:] indices = np.ascontiguousarray(matrix.indices, dtype=np.long).data
+    cdef double [:] data = np.ascontiguousarray(matrix.data, dtype=np.double).data
+    mat.pointr = &indptr[0]
+    mat.rowind = &indices[0]
+    mat.value = &data[0]
 
     srec = svdLAS2A(&mat, k)
-    #cdef view.array Ut = view.array()
-    #Ut.data = srec.Ut.value[0]
-    #Ut.callback_free_data = free
-    Ut = <double[:srec.d, :srec.Ut.cols]> srec.Ut.value[0]
-    Ut.callback_free_data = free
-    _Ut = np.array(Ut, copy=False)
 
-    Vt = <double[:srec.d, :srec.Vt.cols]> srec.Vt.value[0]
-    Vt.callback_free_data = free
-    _Vt = np.array(Vt, copy=False)
+    p_Ut = <double[:srec.d, :srec.Ut.cols]> srec.Ut.value[0]
+    p_Ut.callback_free_data = free
+    Ut = np.array(p_Ut, copy=False)
 
-    s = <double[:srec.d]> srec.S
-    s.callback_free_data = free
-    _s = np.array(s, copy=False)
+    p_Vt = <double[:srec.d, :srec.Vt.cols]> srec.Vt.value[0]
+    p_Vt.callback_free_data = free
+    Vt = np.array(p_Vt, copy=False)
 
+    p_s = <double[:srec.d]> srec.S
+    p_s.callback_free_data = free
+    s = np.array(p_s, copy=False)
+
+    # this was malloc'ed by svdLAS2A
     free(srec.Ut.value)
     free(srec.Ut)
     free(srec.Vt)
     free(srec.Vt.value)
     free(srec)
 
-    return (_Ut, _s, _Vt)
+    return Ut, s, Vt
